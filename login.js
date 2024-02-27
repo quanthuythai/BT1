574f9671b420326a0666eea71904e95880cea921
quanthuythai <126771177+quanthuythai@users.noreply.github.com>
2024-02-27 09:45:12 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,21 @@
+document.getElementById("form_login").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    var loginUser = document.getElementById("loginUser").value;
+    var loginPass = document.getElementById("loginPass").value;
+    
+    if (loginUser === "") {
+        showError("Vui lòng nhập tên đăng nhập");
+    } else if (loginPass === "") {
+        showError("Vui lòng nhập mật khẩu");
+    } else {
+        alert("Đăng nhập thành công");   
+    }
+});
+
+
+function showError(errorMessage) {
+    var errorElement = document.createElement("p");
+    errorElement.classList.add("error");
+    errorElement.textContent = errorMessage;
+    document.body.appendChild(errorElement);
+}
