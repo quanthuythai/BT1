2fca5e46f601088df2baf830391f1643f79c5855
Shawan00 <158188472+Shawan00@users.noreply.github.com>
2024-03-04 21:07:37 +0700

    Add files via upload

---
+++ outcome.js
@@ -0,0 +1,10 @@
+var ketQua = document.getElementById('ketQua');
+// var soCauHoi = document.getElementById('soCauHoi').value;
+var hienThi = document.createElement('div');
+hienThi.classList.add('hienThi');
+hienThi.innerHTML = `
+    <h2>Kết quả</h2>
+    <p>Trả lời đúng ... câu / Tổng số ... câu</p>
+    <p>Điểm số: ...</p>
+    <button>Xem lại bài thi</button>`;
+ketQua.appendChild(hienThi);
